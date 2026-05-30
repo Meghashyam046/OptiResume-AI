81a3898ae044fb71a9a8aa1e38dfb784a61118a7
Meghashyam <shyamroyal916kdm@gmail.com>
2026-05-30 18:55:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://optiresume-ai.onrender.com";
 import { ResumeData, AnalysisResult, AIHistoryItem } from "./types";
 import { ResumeForm } from "./components/ResumeForm";
 import { AnalysisDisplay } from "./components/AnalysisDisplay";
